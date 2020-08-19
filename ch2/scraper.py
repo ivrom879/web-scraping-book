483a7386538de9836d586b8d2327e35133d04205
ivrom879 <ivrom879@yandex.ru>
2020-08-19 19:06:47 +0300

    updated ch2

--- ch2/scraper.py
+++ ch2/scraper.py
@@ -1,4 +1,8 @@
 from urllib.request import urlopen
-from urllib.error import HTTPError
 from bs4 import BeautifulSoup
-html = urlopen('http://www.pythonscraping.com/pages/warandpeace.html')
+
+html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+bsObj = BeautifulSoup(html, "html.parser")
+
+for child in bsObj.find("table", {"id": "giftList"}).tr.next_siblings:
+    print(child)
